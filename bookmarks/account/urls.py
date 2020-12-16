ab341c605cba99753edcb76e653059a35e1a9c8d
wiky-avis <peta-holding@yandex.ru>
2020-12-16 10:28:51 +0300

    Обработчики смены пароля

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -4,6 +4,16 @@ from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
+    # Шаблоны для доступа к обработчикам смены пароля.
+    path(
+        'password_change/', 
+        auth_views.PasswordChangeView.as_view(), name='password_change'
+        ),
+    path(
+        'password_change/done/', 
+        auth_views.PasswordChangeDoneView.as_view(), 
+        name='password_change_done'
+        ),
     # Обработчики действий со статьями.
     #path('login/', views.user_login, name='login'),
     # авторизация пользователей посредством системы аутентификации Django.
