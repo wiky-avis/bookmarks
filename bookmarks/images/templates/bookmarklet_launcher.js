c0da764d38e146e6162d18da6538b98bd85b668c
wiky-avis <peta-holding@yandex.ru>
2020-12-17 22:55:44 +0300

    222

--- bookmarks/images/templates/bookmarklet_launcher.js
+++ bookmarks/images/templates/bookmarklet_launcher.js
@@ -1,11 +1,8 @@
 (function(){
-    if (window.myBookmarklet !== undefined){
+  if (window.myBookmarklet !== undefined){
       myBookmarklet();
-    }
-    else {
-    document.body.appendChild(
-      document.createElement('script')
-    ).src='https://abc4fcd96977.ngrok.io/static/js/bookmarklet.js?r=' +
-      Math.floor(Math.random()*99999999999999999999);
-    }
-  })();
+  }
+  else {
+      document.body.appendChild(document.createElement('script')).src='https://1a5476e52cf9.ngrok.io/static/js/bookmarklet.js?r='+Math.floor(Math.random()*99999999999999999999);
+  }
+})();
