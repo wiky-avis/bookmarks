c04810d7dc8b6b1cf301a1752b7f39c063a4aff2
wiky-avis <peta-holding@yandex.ru>
2020-12-18 13:28:50 +0300

    Реализация системы поДписок.Отношение «многие ко многим» с промежуточной моделью.

--- bookmarks/account/models.py
+++ bookmarks/account/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+from django.contrib.auth.models import User
 
 
 # Расширение модели пользователя
@@ -14,3 +15,24 @@ class Profile(models.Model):
 # Поле photo имеет тип ImageField. Для работы с ним нам необходимо установить 
 # библиотеку изображений Pillow - pip install Pillow
 
+
+# для связи пользователей
+class Contact(models.Model):
+    user_from = models.ForeignKey(
+        'auth.User', related_name='rel_from_set', on_delete=models.CASCADE
+        )
+    user_to = models.ForeignKey(
+        'auth.User', related_name='rel_to_set', on_delete=models.CASCADE
+        )
+    created = models.DateTimeField(auto_now_add=True, db_index=True)
+
+    class Meta:
+        ordering = ('-created',)
+
+    def __str__(self):
+        return '{} follws {}'.format(self.user_from, self.user_to)
+
+# Динамическое добавление поля following в модель User
+User.add_to_class('following', models.ManyToManyField(
+    'self', through=Contact, related_name='followers', symmetrical=False
+    ))
