eb6d7b2c2119a7a19c127a6b5fc06b82f5a8fac3
wiky-avis <peta-holding@yandex.ru>
2020-12-18 14:17:22 +0300

    AJAX-обработчик для создания подписчика

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -45,5 +45,6 @@ urlpatterns = [
     # рабочий стол
     path('', views.dashboard, name='dashboard'),
     path('users/', views.user_list, name='user_list'),
+    path('users/follow/', views.user_follow, name='user_follow'),
     path('users/<username>/', views.user_detail, name='user_detail'),
 ]
